03ecd6b69078808d7a4954466f5d497bac0a3b8b
hamza aboras <hamzaaboras@hotmail.com>
2021-03-31 23:27:43 +0300

    hamza_test

--- python-communication-test/hamza_test.py
+++ python-communication-test/hamza_test.py
@@ -11,9 +11,10 @@ import tensorflow_probability as tfp
 num_inputs = 4
 num_actions = 4
 num_hidden = 128
-
-
-class model(tf.keras.Model):
+gamma=0.99
+opt = keras.optimizers.Adam(learning_rate=0.01)
+class Model(tf.keras.Model):
+  
   def __init__(self):
     super().__init__()
     self.d1 = tf.keras.layers.Dense(30,activation='relu')
@@ -28,42 +29,45 @@ class model(tf.keras.Model):
     return x
 
 
-  def act(self,state):
-    prob = (np.array([state]))
-    dist = tfp.distributions.Categorical(probs=prob, dtype=tf.float32)
-    action = dist.sample()
-    return int(action.numpy()[0])
+model = Model()
+
+
+def act(state):
+  prob = model(np.array([state]))
+  dist = tfp.distributions.Categorical(probs=prob, dtype=tf.float32)
+  action = dist.sample()
+  return int(action.numpy()[0])
 
   
-  def train(self, states, rewards, actions):
-    sum_reward = 0
-    discnt_rewards = []
-    rewards.reverse()
-    for r in rewards:
-      sum_reward = r + self.gamma*sum_reward
-      discnt_rewards.append(sum_reward)
-    discnt_rewards.reverse()  
+def train(states, rewards, actions):
+  sum_reward = 0
+  discnt_rewards = []
+  rewards.reverse()
+  for r in rewards:
+    sum_reward = r + gamma*sum_reward
+    discnt_rewards.append(sum_reward)
+  discnt_rewards.reverse()  
 
-    for state, reward, action in zip(states, discnt_rewards, actions):
-      with tf.GradientTape() as tape:
-        p = self.model(np.array([state]), training=True)
-        loss = self.a_loss(p, action, reward)
-      grads = tape.gradient(loss, self.model.trainable_variables)
-      self.opt.apply_gradients(zip(grads, self.model.trainable_variables))
+  for state, reward, action in zip(states, discnt_rewards, actions):
+    with tf.GradientTape() as tape:
+      p = model(np.array([state]), training=True)
+      loss = a_loss(p,action,reward)
+    grads = tape.gradient(loss, model.trainable_variables)
+    opt.apply_gradients(zip(grads, model.trainable_variables))
 
 
 
   
-  def a_loss(self,prob, action, reward): 
-    dist = tfp.distributions.Categorical(probs=prob, dtype=tf.float32)
-    log_prob = dist.log_prob(action)
-    loss = -log_prob*reward
-    return loss 
+def a_loss(prob, action, reward): 
+  dist = tfp.distributions.Categorical(probs=prob, dtype=tf.float32)
+  log_prob = dist.log_prob(action)
+  loss = -log_prob*reward
+  return loss 
 
 
 unity = Unity()
-agentoo7 = model()
-steps = 500
+
+steps = 200
 for s in range(steps):
   
   done = False
@@ -74,7 +78,7 @@ for s in range(steps):
   actions = []
   while not done:
     #env.render()
-    action =agentoo7.act(state)
+    action =act(state)
     #print(action)
     next_state,reward,done= unity.action(action)
     
@@ -89,6 +93,6 @@ for s in range(steps):
     total_reward += reward
     
     if done:
-      agentoo7.train(states, rewards, actions)
+      train(states, rewards, actions)
       #print("total step for this episord are {}".format(t))
-      print("total reward after {} steps is {}".format(s, total_reward))
+      print("total reward = {} episd = {}".format(total_reward,s))
