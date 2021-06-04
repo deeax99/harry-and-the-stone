4b95ac56e5632569363aa57effaad1da32682bbf
Deea Majed <deea1887@yahoo.com>
2021-06-04 19:32:25 +0300

    feat: reward/lose/win/max frames

--- python-communication-test/a.py
+++ python-communication-test/a.py
@@ -3,18 +3,16 @@ env = Unity()
 
 limited = 0
 while (True):
-    print("start state",env.reset())
+    print("start state", env.reset())
     while (True):
         action = {}
-        Unity.apply_second_thieve_action(action ,1,0,1)
-        if (limited % 1000 == 0):
-            print("test",env.reset())
-            
-        state , _ , _ = env.action(action)
 
-            
+        Unity.apply_second_thieve_action(action, 1, 0, 1)
+        if (limited % 1000 == 0):
+            print("test")
         limited += 1
-        if limited == 1000:
-            print("reset")
+        state, rewards, dones = env.action(action)
+        if dones[0]:
+            print(dones , rewards)
+            env.reset()
             limited = 0
-            
